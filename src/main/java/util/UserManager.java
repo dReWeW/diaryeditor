e9aa18abd8247dacc26938b29965d198cd28c28e
dReWeW <heybiubi@outlook.com>
2020-12-16 16:30:29 +0800

    Usermanager ok

--- src/main/java/util/UserManager.java
+++ src/main/java/util/UserManager.java
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-12-14 03:46:09
- * @LastEditTime: 2020-12-15 23:30:21
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-12-16 15:25:17
+ * @LastEditors: 郑浩龙-2018141493022
  * @Description: 控制user信息与数据库的链接
  * @FilePath: \dirayEditor\src\main\java\\util\UserManager.java
  */
@@ -12,8 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
-
-import javax.xml.namespace.QName;
 
 public class UserManager {
     private static String driverClass, url, userName, passWord;
@@ -32,17 +30,48 @@ public class UserManager {
         }
     }
 
-    public static void readUser() throws IOException {
+    public static boolean readUser(String username, String password) throws IOException {
         try {
             Class.forName(driverClass);
             con = DriverManager.getConnection(url, userName, passWord);
+            Statement stmt = con.createStatement();
+            String sql = String.format(
+                    "SELECT UserName,UserPassword from USERS WHERE UserName='%s' AND UserPassword=%s", username,
+                    password);
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {// 如果有查询结果，直接允许登录
+                con.close();
+                rs.close();
+                stmt.close();
+                return true;
+            } else {
+                con.close();
+                rs.close();
+                stmt.close();
+                return false;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return true;
+
     }
 
-    public static void writeUser() throws IOException {
+    public static void writeUser(String username, String password) throws IOException {
+        try {
+            Class.forName(driverClass);
+            con = DriverManager.getConnection(url, userName, passWord);
+            Statement stmt = con.createStatement();
+            String sql = String.format("INSERT INTO USERS VALUES(%s,%s)", username, password);
+            // ResultSet rs = stmt.executeQuery(sql);
+            stmt.executeUpdate(sql);
+            con.commit();
+            con.close();
+            stmt.close();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
